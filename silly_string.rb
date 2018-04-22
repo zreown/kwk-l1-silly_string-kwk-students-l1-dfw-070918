861cb04f286d938fdd88357a19e2e9764366c5f4
aviflombaum <avi@flombaum.com>
2018-04-21 20:09:17 -0400

    fix

--- silly_string.rb
+++ silly_string.rb
@@ -17,7 +17,7 @@ phrase = "IM NOT SHOUTING"
 
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
-puts phrase
+puts lowercased_phrase
 
 # Challenge 4: Count how many letters are in that big_word
 
